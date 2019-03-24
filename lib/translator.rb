38eead81de64efab46c75ea99ddf23d4837bd6ec
Reinald Reynoso <reireynoso@gmail.com>
2019-03-24 23:31:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,9 +10,10 @@ def load_library(path)
   puts new_hash[0]
   emoticons = YAML.load_file(path)
   emoticons.each do |definition,language_array|
-  
+   
    new_hash['get_meaning'][language_array[1]] = definition
-   new_hash['get_emoticon'][language_array[0]] = new_hash['get_meaning'][language_array[1]]
+   puts  new_hash['get_meaning'][language_array[1]]
+   #new_hash['get_emoticon'][language_array[0]] = 
    #language_array.each_with_index do |val,index|
     # if index == 0
     #   new_hash['get_emoticon'][val] = definition
@@ -21,7 +22,7 @@ def load_library(path)
     # end 
    #end
   end
-  puts new_hash
+  #puts new_hash
   return new_hash
 end
 
