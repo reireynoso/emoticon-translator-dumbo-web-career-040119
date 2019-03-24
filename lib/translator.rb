f9635ad8b96cd0dc4799e65fd736ba140ac240d3
Reinald Reynoso <reireynoso@gmail.com>
2019-03-24 23:30:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,14 +12,14 @@ def load_library(path)
   emoticons.each do |definition,language_array|
   
    new_hash['get_meaning'][language_array[1]] = definition
-   new_hash['get_emoticon'][language_array[0]] =  new_hash['get_meaning'][language_array[1]]
+   new_hash['get_emoticon'][language_array[0]] = new_hash['get_meaning'][language_array[1]]
    #language_array.each_with_index do |val,index|
     # if index == 0
     #   new_hash['get_emoticon'][val] = definition
     # else
     #   new_hash['get_meaning'][val] = new_hash['get_emoticon'][val]
     # end 
-   end
+   #end
   end
   puts new_hash
   return new_hash
